8e241de284f5d8dba4fb8995b9f04129e8ffb12c
Zach Liibbe <zliibbe@gmail.com>
2022-04-06 10:08:33 -0600

    Add Map through tricks to display to DOM

--- src/components/App/TrickContainer.js
+++ src/components/App/TrickContainer.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import Trick from './Trick'
 
-const TrickContainer = () => {
-    return (
-//      Map through available tricks to display each of them
+const TrickContainer = (props) => {
+    const tricks = props.Tricks.map(trick => {
+        return (
+            <Trick 
+            name={trick.name}    
+            stance={trick.stance}
+            obstacle={trick.obstacle}
+            tutorial={trick.tutorial}
+            key={trick.key}
+            />
         )
+    })
+    
+    return (
+        <section className='trick-container'>
+            {tricks}
+        </section>
+    )
 }
 
 
