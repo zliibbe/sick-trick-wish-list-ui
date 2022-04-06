6cf470015232ead4da8f38aae037cf08d6ec7c30
Zach Liibbe <zliibbe@gmail.com>
2022-04-06 11:07:10 -0600

    Remove data file import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import TrickContainer from './TrickContainer';
-// import tricksData from './tricksData'
 
 
 class App extends Component {
