dac712540302dcb540567ff37f4e3c090bc7035f
Zach Liibbe <zliibbe@gmail.com>
2022-04-06 09:57:41 -0600

    Add tricks data for page display

---
+++ src/components/App/tricksData.js
@@ -0,0 +1,18 @@
+const tricksData = [
+    {
+        "stance": "regular",
+        "name": "treflip",
+        "obstacle": "flat ground",
+        "tutorial": "https://www.youtube.com/watch?v=XGw3YkQmNig",
+        "id": 1
+    },
+    {
+        "stance": "switch",
+        "name": "heelflip",
+        "obstacle": "stairs",
+        "tutorial": "https://www.youtube.com/watch?v=9N9swrZU1HA",
+        "id": 2
+    }
+]
+
+export default tricksData
